9c56ec99675316bd6abd26e110bca0fd26e34b43
MhankBarBar <mhankbarbar@pm.me>
2026-04-27 18:18:08 +0700

    chore: scaffold bun sveltekit project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host ?? '0.0.0.0',
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421
+        }
+      : undefined
+  },
+  envPrefix: ['VITE_', 'TAURI_']
+});
